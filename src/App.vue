b367ca747b081d9735f9ab001a64f00673402cf0
davidscheer58 <122395831+davidscheer58@users.noreply.github.com>
2023-02-06 12:17:53 -0500

    Home Work 4

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+<button class="dupe">Duplication glitch</button>
+
+<button class="delete">Delete</button>
+
+ 
+    <button id="btn1">Change the color</button>
+
+
+
+
+<div class="card">
+    <img src="https://variety.com/wp-content/uploads/2019/06/spongebob-battle-remaster.png?w=681&h=383&crop=1" />
+    <p class="card_title title-white">Project 1 complete</p>
+  
+  <details>
+    <summary>Details</summary>
+    This card was created by David Scheer, this was a struggle but like Spongebob, we perservered and compleated it. In conclusion this wonderful picture is of spongebob with the title of Project 1 complete underneath is the details button I created that links to the required website.
+</details>
+ 
+</div>
 </template>
 
 <script setup>
@@ -20,3 +38,68 @@ import HelloWorld from './components/HelloWorld.vue'
   margin-top: 60px;
 }
 </style>
+
+<style> 
+.card {
+  width: 600px;
+  height: 600px;
+  border: 5px solid black;
+  margin: 20px;
+  padding: 20px;
+   background-color: rgb(120,45,99);
+    text-align: center;
+}
+
+img {
+  width: 100%;
+}
+
+.card_title {
+  text-align: left;
+  font-family: sans-serif;
+  font-weight: bold;
+  font-size: 25px;
+}
+
+.card_paragraph{
+  Text-align: left;
+  font-family: sans-serif;
+  font-weight: bold; 
+  font-size: 15px; 
+  
+}
+
+@media screen and (max-width: 500px){
+  .card {
+    width: 100%;
+    border: 5px solid red;
+    margin: 20px;
+    padding: 20px;
+  }
+  
+ .btn {
+    margin: 1px 10px 50px; 
+    border: 2px solid red; 
+    text-transform: uppercase; 
+    padding: 8px 8px; 
+   }
+
+  .card_paragraph {
+    visibility: hidden;
+  }
+}
+
+
+
+</style>
+
+
+
+
+  
+
+  
+ 
+  
+
+
--- src/main.js
+++ src/main.js
@@ -2,3 +2,35 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+document.querySelector('.dupe').addEventListener('click', function(e) {
+    var card = document.querySelector('.card');
+      var dupCard = card.cloneNode(true);
+      console.log(dupCard);
+      document.body.appendChild(dupCard);
+    });
+    
+    
+    changeColor = document.querySelector('#btn1');
+    let body = document.querySelector('body');
+    changeColor.addEventListener('click', function(){
+        let r = (Math.floor(Math.random() * 256));
+        let g = (Math.floor(Math.random() * 256));
+        let b = (Math.floor(Math.random() * 256));
+        
+        body.style.backgroundColor = `rgb(${r},${g},${b})`;
+    });
+    
+    document.querySelector('.delete').addEventListener('click', function(e) {
+    var card = document.querySelector('.card');
+      var dupCard = card.cloneNode(true);
+      console.log(dupCard);
+      document.body.appendChild(dupCard);
+    });
+    
+    
+    
+    
+    
+    
+    
